e6539c946d420455439da7a6da9dc9e8d932ef6c
Kaur Lumiste <kaur@salv.com>
2021-02-03 15:51:42 +0200

    Moodle VPL specific result calculation

--- vpl/testthat/homework_check_functions.R
+++ vpl/testthat/homework_check_functions.R
@@ -6,71 +6,64 @@
 # Used for:           Automatic testing of homeworks
 #***************************************************************************************************/
 
-apply_tests <- function(filename, tests_file){
+comment <- function(string){
+  # formats strings to create VPL comments
+  cat(paste0('Comment :=>> ', string, '\n')) 
+}
+
+grade <- function(num){
+  # formats strings to create VPL comments
+  cat(paste0('Grade :=>> ', num, '\n')) 
+}
+
+homework_check <- function(.tests_file, .tests_structure){
   #' '''''
-  #' @description Function that initiates tests using testthat package. Requires a file to test and 
+  #' @description Initiates unit tests using testthat package. Requires a file to test and 
   #' file that includes all the tests.
-  #' @param 'filename' - path to the homework submission
-  #' @param 'tests_file' - path to the file that includes all the tests
+  #' @param '.submission' - path to the homework submission
+  #' @param '.tests_file' - path to the file that includes all the tests
   #' '''''
+  
+  # Apply tests
+  output <- test_file(.tests_file, reporter = SilentReporter) %>% 
+    as.data.frame()
+  
+  # Calculate grade
+  output %>%
+    summarise(grade = round((1 - sum(failed) / sum(nb))*100)) %>% 
+    grade()
+    
+  # Tests skipped
+  if(any(output$skipped)){
+    comment('Syntax errors!')
 
-  # Create a new enviroment
-  #tstEnv <- new.env()
-  
-  # Read file
-  #read.file = try(sys.source(file = filename, envir = tstEnv, toplevel.env = tstEnv))
-  read.file = try(source(filename, encoding = "UTF-8", verbose = F, echo = F, print.eval = F, max.deparse.length = 1))
-  
-  # If error in reading file, then exit function. Else run tests
-  if(class(read.file)=="try-error") {
-    
-    message(paste0("FAILED: Reading file ", filename, " - FAILED!"))
-    return(NULL)
-    break
-    
-  } else {
-    
-    test_results <- test_file(tests_file, reporter = SilentReporter) %>% 
-      as.data.frame() %>% 
-      mutate(submission = filename) %>% 
-      select(submission, everything())
-    
-    return(test_results)
+    output %>% filter(skipped) %>%
+      rowwise() %>% 
+      mutate(message = unlist(result, recursive = F)[[1]],
+             message = paste0('VIGA - ', test, '\n', message)) %>% 
+      select(message) %>% 
+      pull() %>% 
+      comment()
   }
-}
-
-
-homework_check <- function(.submission, .tests_file, .tests_structure){
-
-  # Clean environment from previous results
-  rm(list=ls()[!ls() %in% c("filename", 'homework_check')])
+  
+  # Tests failed
+  if(sum(output$failed) > 0){
+    comment('Some mistakes were fount in your solution')
     
-  # Apply tests in a separate environment
-  .output <- apply_tests(.submission, .tests_file)
-    
-  
-  # Recast and format the results to show only relevant info
-  final_output <- .output %>% 
-    dcast(context + submission ~ test, value.var = "failed") %>% 
-    mutate(Total = rowSums(.[-c(1:2)])) 
-  
-  colnames(.tests_structure) <- colnames(final_output)
-  final_output[, 3:ncol(.tests_structure)] <-
-    round((1 - mapply("/", final_output[, 3:ncol(final_output)], .tests_structure[3:ncol(.tests_structure)]))  *
-            100, 1) 
-  
-  final_output <- rbind(.tests_structure, final_output)
-  
-  final_output <- final_output %>% 
-    left_join(.output %>% 
-                mutate(test = paste0(test,"_error")) %>% 
-                dcast(context + submission ~ test, value.var = "error"))
-  
+    output %>% 
+      filter(failed > 0 & !skipped) %>%
+      rowwise() %>% 
+      mutate(message = unlist(result, recursive = F)[[1]],
+             message = paste0('VIGA - ', test, '\n', message)) %>% 
+      select(message) %>% 
+      pull() %>% 
+      comment()
+  }
   
 }
 
 
-code_run_test <- function(code){
+code_run_test <- function(code, ylesanne = NULL){
   #' '''''
   #' @description Tries to run code and if it fails, then return error and skip all tests .
   #' @param 'code' - character vector of code lines to run
