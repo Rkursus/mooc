cc312da98000a10070cccf1ebfedeb4b61bde958
Kaur Lumiste <kaur@salv.com>
2021-02-05 19:04:07 +0200

    Update kodutoo1_pohi.R

--- docs/kodutood/kodutoo1/kodutoo1_pohi.R
+++ docs/kodutood/kodutoo1/kodutoo1_pohi.R
@@ -22,7 +22,7 @@
 
 # Näide 1. Omistame muutjale y väärtuse 2 ja väljastame väärtuse
 
-y <- 2
+y = 2
 y
 
 # Näide 2. Kasutame muutujat y arvutuses
@@ -64,22 +64,22 @@ y + 5
 
 # Näide 1: omistame muutujale x väärtuseks  teksti "Tere maailm!" ja väljastame selle
 
-x <- "Tere maailm!"
+x = "Tere maailm!"
 x
 
 # Näide 2: tekstide ühendamine, eri võimalusi
 
-poisse <- "kolm"
-tydrukuid <- 2
+poisse = "kolm"
+tydrukuid = 2
 paste(poisse, "ja", tydrukuid)
 paste(poisse, tydrukuid)
 paste(poisse, tydrukuid, sep = "")
 
 # Näide 3: tekste ei saa liita, tulemuseks on veateade
 
-poisse <- "kolm"
-tydrukuid <- 2
-lapsi <- poisse + tydrukuid
+poisse = "kolm"
+tydrukuid = 2
+lapsi = poisse + tydrukuid
 
 
 # 2.4.1 ÜLESANNE
@@ -94,8 +94,8 @@ lapsi <- poisse + tydrukuid
 # Näide 1: Moodustame 2 vektorit, millest ühes on kirjas temperatuurid (20.01.2010 kell 10), 
 # teises ilmajaamad, kus need on mõõdetud:
 
-temp <- c(-6.2, -12.9, -13.0, -15.4, -16.1, -16.9, -17.0, -19.6, -19.9)
-jaam <- c("Ruhnu", "Kihnu", "Pakri", "Tallinn", "Pärnu", "Kunda", "Kuusiku", "Võru", "Jõgeva")            
+temp = c(-6.2, -12.9, -13.0, -15.4, -16.1, -16.9, -17.0, -19.6, -19.9)
+jaam = c("Ruhnu", "Kihnu", "Pakri", "Tallinn", "Pärnu", "Kunda", "Kuusiku", "Võru", "Jõgeva")            
 
 # Näide 2: Väljastame tulemused ekraanile
 
@@ -103,7 +103,7 @@ temp; jaam
 
 # Näide 3: Paneme temperatuuridele jaamanimed juurde ja vaatame tulemust
 
-names(temp) <- jaam
+names(temp) = jaam
 temp
 
 
@@ -111,13 +111,13 @@ temp
 
 # Ülesanne 1
 
-Fahrenheit <- ________
+Fahrenheit = ________
 Fahrenheit
 
 # Ülesanne 2
 
-lisa <- ________
-names(lisa) <- ________
+lisa = ________
+names(lisa) = ________
 
 
 # Ülesanne 3  
@@ -173,12 +173,12 @@ temp[jaam == "Tallinn"]  # valime välja Tallinnale vastava temperatuuri
 
 # Ülesanne 2
 
-vastus1 <- ________
+vastus1 = ________
 vastus1
 
 # Ülesanne 3
 
-vastus2 <- ________
+vastus2 = ________
 vastus2
 
 
@@ -186,9 +186,9 @@ vastus2
 
 # Moodustame mõned vektorid ja vaatame tulemust.
 
-muutuja1 <- c("TRUE", "true",  "Tru", "FALSE", "F", "false", NA)
-muutuja2 <- c(1:3, NA, 0, Inf - Inf)
-muutuja3 <- 1:6
+muutuja1 = c("TRUE", "true",  "Tru", "FALSE", "F", "false", NA)
+muutuja2 = c(1:3, NA, 0, Inf - Inf)
+muutuja3 = 1:6
 muutuja1; muutuja2; muutuja3
 
 
@@ -210,24 +210,24 @@ muutuja1; muutuja2; muutuja3
 ###% ---- 2.9 Veel tõeväärtustest 1 ----
 
 # antud on vektor x
-x <- c(34, 23, 45, 67, 10, 21, 37)
+x = c(34, 23, 45, 67, 10, 21, 37)
 
 #Näide 1: Väärtustame tõeväärtusvektori, mille elementide väärtus on `TRUE`, 
 # kui x väärtused on suuremad kui 50. Prindime ekraanile
 
-x1 <- x > 50
+x1 = x > 50
 x1
 
 #Näide 2: Väärtustame tõeväärtusvektori, mille elementide väärtus on `TRUE`, 
 # kui  x väärtused on  väiksemad kui 20. Prindime ekraanile
 
-x2 <- x < 20
+x2 = x < 20
 x2
 
 #Näide 3: Moodustame tõeväärtusvektori, mis näitab, millised x väärtused on 
 # alla 20 või üle 50. Prindime ekraanile
 
-x3 <- x1 | x2 # loogiline tehe 'või'
+x3 = x1 | x2 # loogiline tehe 'või'
 x3
 
 
@@ -254,7 +254,7 @@ x6
 # Näide: vaatame tähestiku algust, moodustame kolme moodi tõevektori, mille väärtus on TRUE, 
 # kui täht on a või b ning on FALSE vastasel korral
 
-abc <- letters[1:3]
+abc = letters[1:3]
 abc
 abc == "a" | abc == "b" # täht on 'a' või täht on 'b'
 abc != "c"  # täht ei ole 'c'
@@ -267,14 +267,14 @@ table(y)
 
 # Jooksuta järgnev kood
 
-kordused <- c(45, 20, 68, 9)
-y <- sample(rep(c("tere", "hei", "tsau", "hommikust"), times = kordused), size = sum(kordused))
+kordused = c(45, 20, 68, 9)
+y = sample(rep(c("tere", "hei", "tsau", "hommikust"), times = kordused), size = sum(kordused))
 
 
 # 2.10.1 ÜLESANNE
 
 # Ülesanne 1
 
-viimane <- ________
+viimane = ________
 viimane
 
