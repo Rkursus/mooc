58997a69129307aacacd60b8fae9052956ec4a22
Kaur Lumiste <kaur@salv.com>
2021-02-07 21:14:45 +0200

    Update kodutoo4_sub.R

--- docs/kodutood/kodutoo4/kodutoo4_sub.R
+++ docs/kodutood/kodutoo4/kodutoo4_sub.R
@@ -10,202 +10,190 @@ library(reshape2)
 jootraha <- tips
 jootraha$day <- factor(jootraha$day, levels = c("Thur", "Fri", "Sat", "Sun"))
 
-# Tutvu andmetega:
-
-summary(jootraha)
-
 
 # 1.1 ÜLESANDED
 
-# Ülesande 1 lahendus
+# Tutvu andmetega
+summary(jootraha)
 
-library(ggplot2)
 
-# Ülesande 2 lahendus
+# Ülesanne 1: aktiveeri pakett
+_______(ggplot2)
 
+# Ülesanne 2: täienda koodi
 j1 <- ggplot(jootraha, aes(x = day)) + 
-  geom_bar(fill = "royalblue")  + 
-  scale_x_discrete(name = "Day of week", 
-                   limits = c("Thur", "Fri", "Sat", "Sun"),
-                   labels = c("thursday", "friday", "saturday", "sunday"))  +
-  ylab(label = "Counts")
+  geom________  + 
+  scale_x____________   +
+  ylab(label = "_________")
 j1
 
-
 ###% ----- 2. Tulpdiagamm osakaaludega -----
-
-# Vaata tunnuste nimed üle (nädalapäev: day, söögiaeg: time):
-
-names(jootraha)
 
 
 # 2.1 ÜLESANDED
 
-# Ülesande 1 lahendus
+# Vaata tunnuste nimed üle (nädalapäev: day, söögiaeg: time)
+names(jootraha)
 
+# Ülesanne : tulpdiagramm osakaaludega
 library(scales)
-
-j1 <- ggplot(jootraha, aes(x = day, fill = time)) + 
-  geom_bar(position = "fill")  +
-  scale_y_discrete(name = "Percentage",
-                   limits = c(0, 1),
-                   labels = c("0%", "100%")) + 
-  scale_fill_hue(name = "Time") +
-  xlab(label = "Day")
+j1 <- ggplot(jootraha, aes(x = ________, ________ = time)) + 
+  geom___________  +
+  scale______________ + 
+  scale______________ +
+  xlab_______ 
 j1
 
 
 ###% ----- 3. Hajuvusdiagramm 1 -----
 
-# Tunnuste nimed andmestikus(total_bill on arve, tip on jootraha dollarites, day nädalapäev):
-
-names(jootraha)
-
 
 # 3.1 ÜLESANDED
 
-# Ülesande 1 lahendus
+# tunnuste nimed andmestikus(total_bill on arve, tip on jootraha dollarites, day nädalapäev)
+names(jootraha)
 
-j1 <- ggplot(jootraha, aes(x = total_bill, y = tip)) + 
-  geom_point(aes(color = day)) +
-  scale_color_hue(limits = c("Thur", "Fri", "Sat", "Sun"))
-
+# Ülesanne1: Hajuvusdiagramm värviliste punktidega
+j1 <- ggplot(jootraha, aes(x = ______, y = _______)) + 
+  geom____________  +
+  scale_______hue(________________)
 j1
 
-# Ülesande 2 lahendus
 
-suurim  <- "Sat"
+# Ülesanne 2: Suurima arvega päev
+suurim  <- "_____"
+
+
 
 
 ###% ----- 4. Hajuvusdiagramm 2 -----
 
-# Tunnuste nimed andmestikus(total_bill on arve, tip on jootraha dollarites, sex sugu, smoker suitsetaja olemasolu):
-
-names(jootraha)
-
 
 # 4.1 ÜLESANDED
 
-# Ülesande 1 lahendus
+# tunnuste nimed andmestikus(total_bill on arve, tip on jootraha dollarites, sex sugu, smoker suitsetaja olemasolu)
+names(jootraha)
 
-j1 <- ggplot(jootraha, aes(x = total_bill, y = tip)) + 
-  geom_point(aes(color = sex, shape = smoker)) 
+# Ülesanne 1: Hajuvusdiagramm värviliste ja eri kujuga punktidega
+j1 <- ggplot(jootraha, aes(x = ______, y = _______)) + 
+  geom____________  
 j1
 
-# Ülesande 2 lahendus
 
-j1 + geom_smooth(method = lm)
+# Ülesanne 2:  Regressioonsirge lisamine
+j1 + ________________________________
+
+
+
 
 
 ###% ----- 5. Histogramm jaotuse iseloomustamiseks -----
 
-# Tunnuste nimed andmestikus (total_bill on arve, tip on jootraha dollarites):
-
-names(jootraha)
-
 
 # 5.1 ÜLESANDED
 
-# Ülesande 1 lahendus
+# tunnuste nimed andmestikus (total_bill on arve, tip on jootraha dollarites)
+names(jootraha)
 
-jootraha$ratio <- jootraha$tip/jootraha$total_bill
+# Ülesanne 1: Uue tunnuse moodustamine
+jootraha$_____ <- _______________
 
-# Ülesande 2 lahendus
 
-ggplot(jootraha, aes(x = ratio, y = ..density..)) + 
-  geom_histogram(bins = 15)
+# Ülesanne 2:  Histogramm (ära muuda 'y = ..density..' osa koodist!)
+ggplot(jootraha, aes(__ = __________, y = ..density..)) + geom_________
 
-# Ülesande 3 lahendus
 
-erind <- "Dinner"
+# Ülesanne 3: Kas erandlik laudkond käis õhtu või lõunasöögil?
+erind <- "_______"
+
+
 
 
 ###% ----- 6. Histogrammi värvi muutmine -----
 
+# Lisame histogrammi joonistamise käsku värvi järgmisel moel:
+
 ggplot(jootraha, aes(x = ratio, y = ..density..)) + 
   geom_histogram(aes(fill = "chartreuse"))
 
-
 # 6.1 ÜLESANDED
 
-# Ülesande 1 lahendus
-
-# Õige vastuse number kommentaarina: 3
+# Ülesanne 1
+# Õige vastuse number
+vastus <- ____
 
 
 ###% ----- 7. Karpdiagrammid -----
 
-# Tunnuste nimed andmestikus (size - laudkonna suurus):
-
-names(jootraha)
-
 
 # 7.1 ÜLESANDED
 
-# Ülesande 1 lahendus
+# tunnuste nimed andmestikus (size - laudkonna suurus)
+names(jootraha)
 
-j1 <- ggplot(jootraha, aes(x = size, y = ratio, group = size)) + 
-  geom_boxplot()
+# Ülesanne 1: Karpdiagramm
+j1 <- ggplot(jootraha, aes(x = _______, y = _______, group = _______)) + geom_________
 j1
 
-# Ülesande 2 lahendus
+# Ülesanne 2: Täienda joonist teljenimedega
+j1 + labs(________, ___________)
 
-j1 + labs(x = "laudkonna suurus", 
-          y = "jootraha ja arve suhe")
-
-# Ülesande 3 lahendus
-
-tabel <- table(jootraha$size)
+# Ülesanne 3: Sagedustabel
+tabel <- ________________
 tabel
+
+
 
 
 ###% ----- 8. Joondiagramm 1 -----
 
-# Tunnuste nimed andmestikus (total_bill on arve, tip on jootraha dollarites, size laudkonna suurus):
-
-names(jootraha)
-
 
 # 8.1 ÜLESANDED
 
-# Ülesande 1 lahendus
+# tunnuste nimed andmestikus (total_bill on arve, tip on jootraha dollarites, size laudkonna suurus)
+names(jootraha)
 
-jootraha$tip.per.person <- jootraha$tip/jootraha$size
+# Ülesanne 1: Uue tunnuse moodustamine
+jootraha$_____ <- _______________
 
-# Ülesande 2 lahendus
 
-ggplot(jootraha, aes(x = size, y = tip.per.person)) + 
-  stat_summary(geom = "line",  fun.y = mean)  + 
+# Ülesanne 2: Joondiagramm
+ggplot(jootraha, aes(x = _______, y = ____________)) + 
+  stat_summary(geom = "_______",  fun = _________)  + 
   scale_x_continuous(breaks = 1:6)
+
+
 
 
 ###% ----- 9. Joondiagramm 2 -----
 
-# Tunnuste nimed andmestikus:
-
-names(jootraha)
-
 
 # 9.1 ÜLESANDED
 
-# Ülesande 1 lahendus
+# tunnuste nimed andmestikus 
+names(jootraha)
 
-ggplot(jootraha, aes(x = size, y = tip.per.person, color = sex)) + 
-  stat_summary(geom = "line",  fun.y = mean)  + 
+
+# Ülesanne : Joondiagramm (soo kaupa)
+ggplot(jootraha, aes(x = _______, y = ____________, ________________)) + 
+  stat_summary(geom = "_______",  fun = _________)  + 
   scale_x_continuous(breaks = 1:6)
+
+
 
 
 ###% ----- 10. Joondiagramm 3 -----
 
-# Tunnuste nimed andmestikus:
-
-names(jootraha)
-
 
 # 10.1 ÜLESANDED
 
-# Ülesande 1 lahendus
+# tunnuste nimed andmestikus 
+names(jootraha)
 
-ggplot(jootraha, aes(x = size, y = tip.per.person, linetype = sex)) + 
-  stat_summary(geom = "line",  fun.y = mean)  + 
+
+# Ülesanne : Joondiagramm (soo kaupa)
+ggplot(jootraha, aes(x = _______, y = ____________, ________________)) + 
+  stat_summary(geom = "_______",  fun = _________)  + 
   scale_x_continuous(breaks = 1:6)
+
+
